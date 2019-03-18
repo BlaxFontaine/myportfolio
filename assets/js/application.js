6fdfb19baf99cd395a2672ed150fd5e62eaeb6eb
Valerie Gehle <valerie.gehle@gmail.com>
2019-03-19 00:04:08 +0100

    dynamic font color change

---
+++ assets/js/application.js
@@ -0,0 +1,9 @@
+var w = Math.round($(document).width() / 255);
+var h = Math.round($(document).height() / 255);
+var body = $(".main-title, i");
+
+$(document).mousemove(function(e){
+    var pageX = Math.round(e.pageX / w);
+    var pageY = Math.round(e.pageY / h);
+    body.css("color", "rgb("+pageX+","+pageY+",255)");
+});
