2183fae72d826e2d136edb9599ecec396a920626
Valerie Gehle <valerie.gehle@gmail.com>
2019-04-01 18:55:50 +0200

    projects layout made responsive and cleanup

--- about.md
+++
@@ -1,6 +0,0 @@
----
-layout: about
-title: BLAX FONTAINE | About
----
-
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde esse, ad quos. Sequi commodi soluta officiis perferendis, rerum hic repellat possimus, optio alias libero laborum aliquam voluptatem sit asperiores veritatis?
--- assets/js/application.js
+++ assets/js/application.js
@@ -4,6 +4,6 @@ var body = $(".main-title, i, .current, .title");
 
 $(document).mousemove(function(e){
     var pageX = Math.round(e.pageX / w);
-    var pageY = Math.round(e.pageY / h);
-    body.css("color", "rgb("+pageX+","+pageY+",255)");
+    var pageY = 255 - Math.round(e.pageY / h);
+    body.css("color", "rgb("+pageX+", "+pageY+",255)");
 });
