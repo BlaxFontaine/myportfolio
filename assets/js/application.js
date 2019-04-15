1adaa0990d57b395f0cd39751654f9c521fbb264
Valerie Gehle <valerie.gehle@gmail.com>
2019-04-15 13:53:44 +0200

    smooth scroll

--- assets/js/application.js
+++ assets/js/application.js
@@ -18,3 +18,23 @@ $(document).mousemove(function(e){
     var pageY = 255 - Math.round(e.pageY / h);
     body.css("color", "rgb("+pageX+", "+pageY+",255)");
 });
+
+
+
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+  $("a[href*='#']:not([href='#'])").click(function() {
+    if (
+      location.hostname == this.hostname
+      && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+      ) {
+        var anchor = $(this.hash);
+        anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+        if ( anchor.length ) {
+          $("html, body").animate( { scrollTop: anchor.offset().top }, 1000).scrollIntoView(true);
+        }
+      }
+  });
+});
