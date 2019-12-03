35facd9de0894d4e46cd9a0351cb04e24b789c78
Blax Fontaine <valerie.gehle@gmail.com>
2019-12-03 17:36:22 +0100

    bootstrap navbar test

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,5 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navigation');
+  const navbar = document.querySelector('.navbar');
   if (navbar) {
     window.addEventListener('scroll', () => {
         navbar.classList.add('nav-black');
@@ -11,7 +11,7 @@ initUpdateNavbarOnScroll();
 
 var w = Math.round($(document).width() / 255);
 var h = Math.round($(document).height() / 255);
-var body = $(".main-title, .icon, .current, .title");
+var body = $(".navbar-brand, .nav-item");
 
 $(document).mousemove(function(e){
     var pageX = Math.round(e.pageX / w);
